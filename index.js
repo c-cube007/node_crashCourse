853958b6768a14a5ab13e83893a86dbd578958ab
Collins Chiekwe <ccctheholyboy@gmail.com>
2024-06-25 12:56:25 +0100

    node js tutorial

---
+++ index.js
@@ -0,0 +1,9 @@
+import { getPosts } from "./postController.js";
+
+console.log
+// const {generateRandomNumber, celciusToFahrenheit} = require('./utils')
+
+// console.log(`Random Number: ${generateRandomNumber()}`)
+
+// console.log(`Celcius to Fahrenheit: ${celciusToFahrenheit(0)}`)
+
